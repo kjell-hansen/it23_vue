146dd97380dad70e3826b2c534f0df03501fb627
ITSTÖD\kjellh <kjell.hansen@gymnasium.ax>
2025-10-06 15:51:36 +0300

    Knapparna flyttade till en egen komponent

--- ssp/src/App.vue
+++ ssp/src/App.vue
@@ -1,64 +1,43 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
+import KnappRad from './components/KnappRad.vue'
 
-
-const score =ref({spelare:0,dator:0})
-const resultat=ref('Du vann!')
-
-function spelarval(e) {
-  let buttons=document.getElementsByClassName('alternativ')
-  for (let b of buttons) {
-    b.classList.remove('spelarval')
-  }
-  e.target.classList.add('spelarval')
-  datorval()
-}
-
-function datorval() {
-  let val=Math.floor(Math.random()*3)
-  let alternativ=['Sten', 'Sax', 'Påse']
-  let buttons=document.getElementsByClassName('alternativ')
-  for (let b of buttons) {
-    b.classList.remove('datorval')
-    b.title=""
-    if (b.textContent == alternativ[val]) {
-      b.classList.add('datorval')
-      b.title="Datorns val"
-    }
-  }
-  hittaVinnare()
-}
+const knappar = ref(['Sten', 'Sax', 'Påse'])
+const score = ref({ spelare: 0, dator: 0 })
+const resultat = ref('Du vann!')
 
 function hittaVinnare() {
-  let buttons=document.getElementsByClassName('alternativ')
+  let buttons = document.getElementsByClassName('alternativ')
   for (let b of buttons) {
-    if(b.classList.contains('spelarval')) {
-      var spelarval=b.textContent
+    if (b.classList.contains('spelarval')) {
+      var spelarval = b.textContent
     }
-    if(b.classList.contains('datorval')) {
-      var datorval=b.textContent
+    if (b.classList.contains('datorval')) {
+      var datorval = b.textContent
     }
   }
 
-  if(spelarval===datorval) {
-    resultat.value="Oavgjort"
-  } else if(spelarval==='Sten' && datorval==='Sax' ||
-    spelarval==='Sax' && datorval==='Påse' ||
-    spelarval==='Påse' && datorval==='Sten'  ) {
-      resultat.value="Du vann"
-      score.value.spelare++
-    } else {
-      resultat.value="Du förlorade"
-      score.value.dator++
-    }
+  if (spelarval === datorval) {
+    resultat.value = 'Oavgjort'
+  } else if (
+    (spelarval === 'Sten' && datorval === 'Sax') ||
+    (spelarval === 'Sax' && datorval === 'Påse') ||
+    (spelarval === 'Påse' && datorval === 'Sten')
+  ) {
+    resultat.value = 'Du vann'
+    score.value.spelare++
+  } else {
+    resultat.value = 'Du förlorade'
+    score.value.dator++
+  }
 }
 
-function reset(){
-  score.value.dator=0
-  score.value.spelare=0
-  let buttons=document.getElementsByClassName('alternativ')
+function reset() {
+  score.value.dator = 0
+  score.value.spelare = 0
+  let buttons = document.getElementsByClassName('alternativ')
   for (let b of buttons) {
-    b.title=""
+    b.title = ''
     b.classList.remove('spelarval')
     b.classList.remove('datorval')
   }
@@ -71,16 +50,14 @@ function reset(){
   </header>
 
   <main>
-    <div class="knapprad">
-      <button class="alternativ" @click="spelarval">Sten</button>
-      <button class="alternativ" @click="spelarval">Sax</button>
-      <button class="alternativ" @click="spelarval">Påse</button>
-    </div>
+    <KnappRad :knappar="knappar" />
     <div class="resultat">
       <p id="resultat">{{ resultat }}</p>
     </div>
     <div class="score">
-      <p><span id="spelare">{{ score.spelare }}</span> - <span id="dator">{{ score.dator }}</span></p>
+      <p>
+        <span id="spelare">{{ score.spelare }}</span> - <span id="dator">{{ score.dator }}</span>
+      </p>
       <button id="nolla" @click="reset">Nollställ poäng</button>
     </div>
   </main>
@@ -91,19 +68,7 @@ header {
   text-align: center;
   margin-bottom: 1.2em;
 }
-button {
-  padding: 0.6em 1.2em;
-  font-size: 1.2em;
-  background-color: #f0f0f0;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  cursor: pointer;
-}
-.knapprad {
-  display: flex;
-  justify-content: center;
-  gap: 0.6em;
-}
+
 .resultat {
   font-size: 1.2em;
   text-align: center;
@@ -113,15 +78,10 @@ button {
   font-size: 1.2em;
   text-align: center;
 }
-button.spelarval {
-  background-color: greenyellow;
-}
-button.datorval{
-  border: red solid 2px;
-}
+
 #nolla {
   margin-top: 2em;
-  padding: .3em .6em;
-  font-size: .8em;
+  padding: 0.3em 0.6em;
+  font-size: 0.8em;
 }
 </style>
---
+++ ssp/src/components/KnappRad.vue
@@ -0,0 +1,53 @@
+<script setup>
+
+const props=defineProps(['knappar'])
+function spelarval(e) {
+  let buttons = document.getElementsByClassName('alternativ')
+  for (let b of buttons) {
+    b.classList.remove('spelarval')
+  }
+  e.target.classList.add('spelarval')
+  datorval()
+}
+
+function datorval() {
+  let val = Math.floor(Math.random() * props.knappar.length)
+  let buttons = document.getElementsByClassName('alternativ')
+  for (let b of buttons) {
+    b.classList.remove('datorval')
+    b.title = ''
+    if (b.textContent == props.knappar[val]) {
+      b.classList.add('datorval')
+      b.title = 'Datorns val'
+    }
+  }
+}
+</script>
+<template>
+  <div class="knapprad">
+    <button v-for="knapp in props.knappar" class="alternativ" :key="knapp" @click="spelarval">
+      {{ knapp }}
+    </button>
+  </div>
+</template>
+<style scoped>
+button.spelarval {
+  background-color: greenyellow;
+}
+button.datorval {
+  border: red solid 2px;
+}
+button {
+  padding: 0.6em 1.2em;
+  font-size: 1.2em;
+  background-color: #f0f0f0;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  cursor: pointer;
+}
+.knapprad {
+  display: flex;
+  justify-content: center;
+  gap: 0.6em;
+}
+</style>
