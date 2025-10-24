e8e202f54d7ad1add84b966cc3224ace0fc8998b
ITSTÖD\kjellh <kjell.hansen@gymnasium.ax>
2025-10-24 13:56:42 +0300

    Lagt till en burger menu

--- weather/src/App.vue
+++ weather/src/App.vue
@@ -1,14 +1,19 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import BurgerMenu from './components/BurgerMenu.vue'
+import { ref } from 'vue'
+
+const menuActive = ref(false)
 </script>
 
 <template>
+  <BurgerMenu @toggle-menu="menuActive = !menuActive" :active="menuActive" />
   <header>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/locations">Locations</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/locations">Locations</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
   </header>
 
   <RouterView />
